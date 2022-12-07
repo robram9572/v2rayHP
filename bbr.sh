1ec9e45c5def70b8e6a609ecbaf713446435fb84
robram9572 <34120952+robram9572@users.noreply.github.com>
2022-12-07 21:20:29 +0800

    Add files via upload

---
+++ bbr.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+# Auth: happylife
+# Desc: v2ray installation script
+# Plat: ubuntu 18.04+
+# Eg  : bash v2ray_installation_vless.sh "你的域名"
+
+echo net.core.default_qdisc=fq >> /etc/sysctl.conf
+echo net.ipv4.tcp_congestion_control=bbr >> /etc/sysctl.conf
+sysctl -p
+sysctl net.ipv4.tcp_available_congestion_control
+lsmod | grep bbr
+
